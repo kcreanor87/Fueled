0e8a08fb6dcb003bfb27d70f9611e9d0e5a8ce11
unknown <kcreanor@hotmail.com>
2017-05-03 07:58:42 +0100

    First push from LD38 repo

---
+++ Fueled/Assets/Resources/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public List<Button> _buttons = new List<Button>();
+    public List<GameObject> _worlds = new List<GameObject>();
+    public GameObject _intro;
+    public GameObject _worldcontainer;
+    public GameObject _levelSelect;
+    public Sprite _mute;
+    public Sprite _unmute;
+    public Image _muteBtn;
+
+    public Text _bestTimeTxt;
+
+    public int _levelSelected;
+
+	// Use this for initialization
+	void Start () {
+        _muteBtn = GameObject.Find("Mute").GetComponent<Image>();
+        _muteBtn.sprite = (AudioListener.pause == true) ? _unmute : _mute;
+        _bestTimeTxt = GameObject.Find("BestTimeTxt").GetComponent<Text>();
+        _bestTimeTxt.text = "Record: " + _playerManager._times[_playerManager._playerLevel].ToString("F2") + "s";
+        for (int i = 0; i < _buttons.Count; i++)
+        {
+            _buttons[i].interactable = (i <= _playerManager._playerLevel);
+        }
+        SelectLevel(_playerManager._playerLevel);
+        _levelSelect.SetActive(false);
+        _worldcontainer.SetActive(false);
+    }
+
+    public void SelectLevel(int i)
+    {
+        _levelSelected = i;
+        for (int j = 0; j < _worlds.Count; j++) {
+            _worlds[j].SetActive(i == j);
+        }
+        _bestTimeTxt.text = "Record: " + _playerManager._times[i].ToString("F2") + "s";
+    }
+
+    public void OpenLevelSelect()
+    {
+        _intro.SetActive(false);
+        _levelSelect.SetActive(true);
+        _worldcontainer.SetActive(true);
+        SelectLevel(_playerManager._playerLevel);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(_levelSelected + 2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        _muteBtn.sprite = (AudioListener.pause == true) ? _unmute : _mute;
+    }
+}
---
+++ Fueled/Assets/Resources/Scripts/PlayerManager/_playerManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _playerManager : MonoBehaviour {
+
+    public static int _playerLevel;
+
+    public static List<float> _times = new List<float>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (!PlayerPrefs.HasKey("PlayerLevel")){
+            PlayerPrefs.SetInt("PlayerLevel", 0);
+            for (int i = 0; i < 12; i++)
+            {
+                PlayerPrefs.SetFloat("Time" + i, 0.0f);
+                _times.Add(0.0f);
+            }
+        }
+        else {
+            _playerLevel = PlayerPrefs.GetInt("PlayerLevel");
+            if (_playerLevel == 12) _playerLevel = 11;
+            for (int i = 0; i < 12; i++)
+            {
+                _times.Add(0.0f);
+            }
+            LoadTimes();
+        }
+    }
+
+    public static void SaveTimes()
+    {
+        for (int i = 0; i < _times.Count; i++)
+        {
+            PlayerPrefs.SetFloat("Time" + i, _times[i]);
+        }
+    }
+
+    void LoadTimes()
+    {
+        for (int i = 0; i < _times.Count; i++)
+        {
+            _times[i] = PlayerPrefs.GetFloat("Time" + i);
+        }
+    }
+}
